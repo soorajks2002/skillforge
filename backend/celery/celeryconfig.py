e8a2f40616e7854d6799403af9a0ab66e332fd6e
soorajks2002 <soorajks2002@gmail.com>
2025-08-02 20:54:51 +0530

    feat(celery): add shell script

--- backend/celery/celeryconfig.py
+++ backend/celery/celeryconfig.py
@@ -2,3 +2,6 @@ broker_url = "amqp://skorhxxz:uLcrKtpLAEc7KBZivsEpmW-Kr8JAlYA4@shrimp.rmq.clouda
 result_backend = "rpc://"
 
 task_time_limit = 100
+
+# How long to keep results in the backend db
+result_expires = 3600 # 1 hour - 60*60 seconds
